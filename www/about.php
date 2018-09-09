fe093a712e3d0b7c5deeb5917186e689410eabb0
Schulz9 <gumenyuksb@ua.fm>
2018-09-09 15:25:04 +0300

    release v1

---
+++ www/about.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+
+<html lang="ru-RU" class="g-offcanvas-css3">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="description" content="НафтоТрейд">
+		<link rel="pingback" href="http://naftotrade/index.php">
+		<title>ООО "НефтеТрэйд Групп" - компания по продаже нефтепродуктов</title>
+		<link rel="stylesheet" id="contact-form-7-css" href="./index_files/styles.css" type="text/css" media="all">
+		<link rel="stylesheet" id="font-awesome.min-css" href="./index_files/font-awesome.min.css" type="text/css" media="all">
+		<link rel="stylesheet" id="nucleus-css" href="./index_files/nucleus.css" type="text/css" media="all">
+		<link rel="stylesheet" id="hydrogen_ntg_main_page-css" href="./index_files/hydrogen_ntg_main_page.css" type="text/css" media="all">
+		<link rel="stylesheet" id="wordpress-css" href="./index_files/wordpress.css" type="text/css" media="all">
+		<link rel="stylesheet" id="style-css" href="./index_files/style.css" type="text/css" media="all">
+		
+	</head>
+    <body class="gantry g-ntg-content-page g-offcanvas-left g-ntgmainsidebar g-style-preset1 page page-id-172 page-template-default site outline-ntg_content_page_nosidebar dir-ltr">
+        <div id="g-page-surround"><div class="g-menu-overlay"></div>
+			<div class="g-offcanvas-hide g-offcanvas-toggle" data-offcanvas-toggle=""><i class="fa fa-fw fa-bars"></i></div>
+			<section id="g-mobilehead">
+                <div class="g-container">                        
+					<div class="g-grid">
+						<div class="g-block visible-phone ntg mobile contacts-module size-100">
+							<div class="g-content">
+								<a href="index.html/#contacts" class="feedback">Залишити заявку</a><a> +38 (068) 717-17-29</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<header id="g-header" class="ntg content-page">
+                <div class="g-container">                        
+					<div class="g-grid">
+						<div class="g-block ntg header-logo-module size-63">
+							<div class="g-content">
+								<a href="/" title="ООО &quot;НафтоТрейд&quot;" rel="home" class="ntg header-logo">
+									<img src="./index_files/big_logo.png" style="width:450px;" alt="ТзОВ &quot;Нафтотрейд&quot;">
+								</a>
+							</div>
+						</div>
+						<div class="g-block hidden-phone ntg contacts-module size-37">
+							<div class="g-content">
+								<a href="./#contacts" class="feedback">Залишити заявку</a>
+								<div class="phone"><span class="caption">+38 (068) 717-17-29</span></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+			<section id="g-navigation">
+                <div class="g-container">
+					<div class="g-grid">
+						<div class="g-block ntg main-page size-100">
+							<div class="g-content">
+								<nav class="g-main-nav" role="navigation">
+									<ul class="g-toplevel">
+										<li class="g-menu-item g-menu-item-type-post_type g-menu-item-5 g-standard   menu-item menu-item-type-post_type menu-item-object-page menu-item-5">
+											<a class="g-menu-item-container" href="./">
+												<span class="g-menu-item-content">
+													<span class="g-menu-item-title">Головна</span>
+													
+												</span>
+											</a>
+										</li>
+										<li class="g-menu-item g-menu-item-type-post_type g-menu-item-174 active g-standard   menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-172 current_page_item menu-item-174">
+											<a class="g-menu-item-container" href="./about.php">
+												<span class="g-menu-item-content">
+													<span class="g-menu-item-title">Про компанію</span>
+												</span>
+											</a>
+										</li>
+										<li class="g-menu-item g-menu-item-88 g-menu-item-link-parent g-menu-item-type-post_type g-parent g-standard menu-item menu-item-88 menu-item-has-children menu-item-object-page menu-item-type-post_type">
+											<a class="g-menu-item-container" href="./products.php">
+												<span class="g-menu-item-content">
+													<span class="g-menu-item-title">Продукція</span>
+												</span>
+											</a>
+											<ul class="g-dropdown g-dropdown-right g-inactive">
+												<li class="g-dropdown-column">
+													<div class="g-grid">
+														<div class="g-block size-100">
+														</div>
+													</div>
+												</li>
+											</ul>
+										</li>
+									</ul>
+								</nav>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section id="g-container-main" class="g-wrapper">
+                <div class="g-container">            
+						<div class="g-block size-100">
+							<main id="g-main">
+								<div class="g-grid">
+									<div class="g-block size-100">
+										<div class="g-content">
+											<div class="platform-content">
+												<div class="content-wrapper">
+													<section class="entry">
+														<article class="post-type-page post-172 page type-page status-publish hentry" id="post-172">
+															<section class="entry-header">
+																<h1 class="entry-title">Про компанію</h1>
+															</section>
+															<section class="entry-content">
+																<p>&emsp;&emsp;&emsp;Наша компанія спеціалізується в сфере оптової поставки різноманітних видів нафтопродуктів на вигідних умовах. Ми пропонуємо своїм клієнтам та партнерам дійсно вигідні умови співробітництва. Це стало можливо завдяки ефективно організованій роботі самої компанії та налагодженій взаємодії з клієнтами.<br>
+																	&emsp;&emsp;&emsp;При роботе с «НафтоТрейд» ваші заявки обробляються максимально швидко, при цьому доставка в регіони ми можемо здійснювати за допомогою своїх засобів. Для цього є широкі технічні можливості.<br>
+																	&emsp;&emsp;&emsp;Займаючись продажем нафтопродуктів, ми надаємо замовникам повні гарантії якості товарів. Одна з наших переваг полягає в тому, що ми здійснюємо оптові поставки від виробника, все пропоновані товари стандартизовані та гарантовано високї якості.<br>
+																&emsp;&emsp;&emsp;Неважливо, які саме нафтопродукти вам потрібно придбати – ви завжди можете звернутися в нашу компанію. Ми оперативно обробимо ваше замовлення та проконсультуємо в найстисліші терміни відносно роботи з нами. Ми намагаємось постійно розширяти свою клієнтску базу, але при цьому спавпрацюємо не тілько з постійними клієнтами, але й разовими замовниками. Співпрацюючи з компанією «НафтоТрейд», ви знаходите надійного партнера.</p>
+															</section>
+														</article>
+													</section>
+												</div> <!-- /content-wrapper -->
+											</div>
+										</div>
+									</div>
+								</div>
+							</main>
+						</div>
+				</div>
+			</section>
+			<footer id="g-footer">
+                <div class="g-container">                        
+					<div class="g-grid">
+						<div class="g-block ntg place size-44">
+							<div class="g-content">
+								<div class="caption">76000, м.Івано-Франківськ<br>вул. Незалежності</div>
+							</div>
+						</div>
+						<div class="g-block ntg e-contacts size-29">
+							<div class="g-content">
+								<div><span> +38 (068) 717-17-29</span></div>
+								<div><span>naftotreid310@ukr.net</span></div>
+							</div>
+						</div>
+						<div class="g-block ntg copyright size-27">
+							<div class="g-content">
+								© ТзОВ "НафтоТрейд"  <br> Все права захищені 2018 г.
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</div>
+		<div id="su-footer-links" style="text-align: center;"></div>
+	</body></html>
